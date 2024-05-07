f96cd3951fda67045ee28fadec58700a9da398af
mohammed hassan <mh2900001@gmail.com>
2024-05-07 11:34:23 +0300

    Complete Tests

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -44,16 +44,39 @@ class ContactTest extends TestCase
     public function test_while_storing_new_contact_name_min_is_3(): void
     {
         $this->postJson(route("contacts.store", ['name' => 'mo']))
+            ->assertJsonValidationErrors(['name'])
             ->json();
     }
-    public function test_while_storing_new_contact_name_max_is_15(): void
+    public function test_while_storing_new_contact_name_max_is_100(): void
     {
-        $this->postJson(route("contacts.store", ['name' => 'mommmkkkjjjlliolk']))
+        $this->postJson(
+            route(
+                "contacts.store",
+                [
+                    'name' => "
+                            Lorem Ipsum is simply dummy
+                            Lorem Ipsum is simply dummy
+                            text of the printing and typesetting industryLorem
+                            Ipsum has been the industrys standard dummy text ever since the
+                            when an unknown printer took a galley of type and scrambled it to make a type
+                            specimen book
+                            It has survived not only five centuries
+                            but also the leap into electronic typesetting
+                            remaining essentially unchanged
+                            It was popularised in the with the release of Letraset sheets containing Lorem Ipsum passages
+                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                            "
+                ]
+            )
+        )
+            ->assertJsonValidationErrors(['name'])
             ->json();
     }
     public function test_while_storing_new_contact_name_is_string(): void
     {
-        $this->postJson(route("contacts.store", ['name' =>  123, 'image' => 'image_src']))->json();
+        $this->postJson(route("contacts.store", ['name' =>  123, 'image' => 'image_src']))
+            ->assertJsonValidationErrors(['name'])
+            ->json();
     }
     public function test_delete_contact()
     {
@@ -69,7 +92,7 @@ class ContactTest extends TestCase
                 'name' => 'update name'
             ]
         );
-        $this->assertDatabaseHas('contacts',[
+        $this->assertDatabaseHas('contacts', [
             'id' => $this->contact->id,
             "name" => "update name"
         ]);
